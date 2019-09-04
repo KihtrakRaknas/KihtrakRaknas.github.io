6e124c32280f66c6288f5311a72b23ba15a829eb
KihtrakRaknas <karthikandsankar@gmail.com>
2019-09-03 20:39:49 -0400

    Updates

---
+++ precache-manifest.ece6ff752cdcdc0dc4740e4373db0662.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcead306a0de00241d2390a80f3c426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ba4f7509bfdcb34642c",
+    "url": "/static/css/main.28e622f8.chunk.css"
+  },
+  {
+    "revision": "796f0e588baa093b0359",
+    "url": "/static/js/2.ab364cfc.chunk.js"
+  },
+  {
+    "revision": "7ba4f7509bfdcb34642c",
+    "url": "/static/js/main.5fbb35df.chunk.js"
+  },
+  {
+    "revision": "afe569a87fce6f65f2c0",
+    "url": "/static/js/runtime~main.ff47fc3e.js"
+  },
+  {
+    "revision": "597709436e5d32586ef722324dba84ee",
+    "url": "/static/media/Karthik.59770943.JPG"
+  },
+  {
+    "revision": "291eccc8761be84c5040d0b9d7572af4",
+    "url": "/static/media/favicon.291eccc8.ico"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
