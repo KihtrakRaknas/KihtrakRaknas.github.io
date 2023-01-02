b59666efb5987fda3d90f480958d61716fbcc44e
KihtrakRaknas <karthikandsankar@gmail.com>
2023-01-02 17:58:04 -0500

    fixed issue where about me card kept resizing

--- src/Sections/Top.js
+++ src/Sections/Top.js
@@ -49,6 +49,7 @@ export default class Top extends React.Component {
                         <div className="p-4 mx-md-5 mx-1 mt-md-1 mt-3" style={{backgroundColor: "#F9F9F9F9", borderRadius: 20, height:"fit-content", boxShadow:"gray 3px 3px 10px", width:"fit-content"}}>
                             <h1 class="display-1 text-center" style={{fontFamily:"Kanit"}}>
                                 <p style={{visibility:'hidden', height:0, margin:0}}>Karthik Sankar|</p>
+                                <p style={{visibility:'hidden', height:0, margin:0}}>Kihtrak Raknas|</p>
                                 {isBot()?"Karthik Sankar":<ReactRotatingText items={['Karthik Sankar', 'Kihtrak Raknas']} />}
                             </h1>
                             <hr/>
